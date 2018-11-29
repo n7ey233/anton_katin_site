574ca14b4cd635591879872b5aa00fdcb0189c17
Your Name <you@example.com>
2018-11-29 17:43:30 +1000

    add

--- app/func.py
+++ app/func.py
@@ -1,11 +1,18 @@
 import requests
-def send_notification_telegram():
-    print('url')
-    id_telegi = '405347178'
-    text = 'text'
+def create_msg(msg_type):
+    #1 == call
+    if msg_type == 1:
+        text = 'пришло уведомление заявки на звонок имя телефон примечание'
+    #2 == order part
+    elif msg_type == 2:
+        text = 'пришло уведомление заявки на запчасть имя телефон запчасть юрл на просмотр'
+    return text
+
+def send_notification_telegram(text):
+    id_telegi = '405347178' #id v telege dlya otpravki
     telega_token = '700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg'
     url = "https://api.telegram.org/bot"+telega_token+"/sendMessage?chat_id="+id_telegi+"&text="+text
-    print(url)
     #r = requests.get('https://api.telegram.org/bot700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg/getUpdates')
     r = requests.get(url)
-    print(r.text)
+    
+send_notification_telegram(create_msg(2))
