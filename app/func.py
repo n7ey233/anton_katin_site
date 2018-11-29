a82b0233f017bcbafc74fcb07490eedaa4680a32
Your Name <you@example.com>
2018-11-29 17:46:20 +1000

    add

--- app/func.py
+++ app/func.py
@@ -1,4 +1,5 @@
 import requests
+#https://www.forsomedefinition.com/automation/creating-telegram-bot-notifications/
 def create_msg(msg_type):
     #1 == call
     if msg_type == 1:
@@ -6,13 +7,18 @@ def create_msg(msg_type):
     #2 == order part
     elif msg_type == 2:
         text = 'пришло уведомление заявки на запчасть имя телефон запчасть юрл на просмотр'
+    print(text)
     return text
+
 
 def send_notification_telegram(text):
     id_telegi = '405347178' #id v telege dlya otpravki
     telega_token = '700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg'
     url = "https://api.telegram.org/bot"+telega_token+"/sendMessage?chat_id="+id_telegi+"&text="+text
     #r = requests.get('https://api.telegram.org/bot700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg/getUpdates')
-    r = requests.get(url)
     
-send_notification_telegram(create_msg(2))
+    #r = requests.get(url)
+msg = create_msg(2)
+print(msg)
+
+#send_notification_telegram(msg)
--- func.py
+++ func.py
@@ -1,9 +1,24 @@
 import requests
+#https://www.forsomedefinition.com/automation/creating-telegram-bot-notifications/
+def create_msg(msg_type):
+    #1 == call
+    if msg_type == 1:
+        text = 'пришло уведомление заявки на звонок имя телефон примечание'
+    #2 == order part
+    elif msg_type == 2:
+        text = 'пришло уведомление заявки на запчасть имя телефон запчасть юрл на просмотр'
+    print(text)
+    return text
 
-print('url')
-id_telegi = '405347178'
-text = 'text'
-telega_token = '700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg'
-url = "https://api.telegram.org/bot"+telega_token+"/sendMessage?chat_id="+id_telegi+"&text="+text
-print(url)
-r = requests.get(url)
+
+def send_notification_telegram(text):
+    id_telegi = '405347178' #id v telege dlya otpravki
+    telega_token = '700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg'
+    url = "https://api.telegram.org/bot"+telega_token+"/sendMessage?chat_id="+id_telegi+"&text="+text
+    #r = requests.get('https://api.telegram.org/bot700264978:AAG6PdQSBamU5nREeT8c07fUzoz5EzNp6Pg/getUpdates')
+    
+    #r = requests.get(url)
+msg = create_msg(2)
+print(msg)
+
+#send_notification_telegram(msg)
